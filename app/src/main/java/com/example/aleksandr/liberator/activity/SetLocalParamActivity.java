e964ac5ff239c88802910f5c5109e7bdb3a20fd4
Aleksandr <aleksandr_mdc@mail.ru>
2016-03-23 02:39:16 +0400

    added local settings

--- app/src/main/java/com/example/aleksandr/liberator/activity/SetLocalParamActivity.java
+++ app/src/main/java/com/example/aleksandr/liberator/activity/SetLocalParamActivity.java
@@ -3,6 +3,8 @@ package com.example.aleksandr.liberator.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.aleksandr.liberator.R;
 import com.example.aleksandr.liberator.static_params.StaticParams;
@@ -14,11 +16,31 @@ public class SetLocalParamActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_local_param);
 
+        initUi();
+
         Intent intent = getIntent();
         Bundle extra = intent.getExtras();
         int fragment = extra.getInt(StaticParams.SHOW_FRAGMENT);
-
+        showNextFragment(fragment);
     }
+
+    private void initUi() {
+        ImageView ibBack = (ImageView) findViewById(R.id.iv_button_back);
+
+        ibBack.setOnClickListener(listener);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.iv_button_back:
+                    onBackPressed();
+                    break;
+            }
+        }
+    };
+
 
     private void showNextFragment(int showStartFragment) {
         switch (showStartFragment) {
