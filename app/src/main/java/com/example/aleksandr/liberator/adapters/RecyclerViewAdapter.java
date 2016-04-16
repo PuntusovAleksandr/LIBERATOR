dfcb55266423bf65484f80518b512f4bd7194553
Aleksandr Puntusov <zloj0811@gmail.com>
2016-04-16 23:27:51 +0400

    added min and max param

--- app/src/main/java/com/example/aleksandr/liberator/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/aleksandr/liberator/adapters/RecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.aleksandr.liberator.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import com.example.aleksandr.liberator.R;
 import com.example.aleksandr.liberator.data_base.Db;
 import com.example.aleksandr.liberator.data_base.entity.EntitySettings;
+import com.example.aleksandr.liberator.static_params.StaticParams;
 import com.example.aleksandr.liberator.utils.Utils;
 
 import java.util.List;
@@ -78,10 +80,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                             if (paramValue > paramMin) {
                                 --paramValue;
                                 holder.tvValues.setText(paramValue + "");
+                            } else {
+                                Snackbar.make(v, R.string.is_min_value, Snackbar.LENGTH_SHORT).show();
                             }
                         } else {
                             if (!textValue.equals(entitySettings.getMinValue())) {
-
+                                String[] massValue = getStringMass(entitySettings.getTitle());
+                                int numberValue = -1;
+                                for (int i = 0; i < massValue.length; i++) {
+                                    if (textValue.equals(massValue[i])) {
+                                        numberValue = i;
+                                    }
+                                }
+                                if (!textValue.equals(massValue[0]) && numberValue != -1) {
+                                    holder.tvValues.setText(massValue[numberValue - 1]);
+                                }
+                            } else {
+                                Snackbar.make(v, R.string.is_min_value, Snackbar.LENGTH_SHORT).show();
                             }
                         }
                         break;
@@ -93,16 +108,31 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                             if (paramValue < paramMax) {
                                 ++paramValue;
                                 holder.tvValues.setText(paramValue + "");
+                            } else {
+                                Snackbar.make(v, R.string.is_max_value, Snackbar.LENGTH_SHORT).show();
                             }
                         } else {
                             if (!textValue.equals(entitySettings.getMaxValue())) {
-
+                                String[] massValue = getStringMass(entitySettings.getTitle());
+                                int numberValue = -1;
+                                for (int i = 0; i < massValue.length; i++) {
+                                    if (textValue.equals(massValue[i])) {
+                                        numberValue = i;
+                                    }
+                                }
+                                if (!textValue.equals(massValue[massValue.length - 1]) && numberValue != -1) {
+                                    holder.tvValues.setText(massValue[numberValue + 1]);
+                                }
+                            } else {
+                                Snackbar.make(v, R.string.is_max_value, Snackbar.LENGTH_SHORT).show();
                             }
                         }
                         break;
 
                     case R.id.iv_set_ok:
                         Db.getInstance(mContext).setParamByEntity(textValue, entitySettings.getTitle());
+                        Snackbar.make(v, "Параметр сохранен", Snackbar.LENGTH_SHORT).show();
+
                         break;
 
                 }
@@ -118,6 +148,35 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
 
     }
+
+    private String[] getStringMass(String mTitle) {
+
+        if (mTitle.equals(mContext.getResources().getString(R.string.auto_start))) {
+            return StaticParams.AUTO_START_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.mode))) {
+            return StaticParams.MODE_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.fuel_loading))) {
+            return StaticParams.LOAD_FUEL_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.system_burn))) {
+            return StaticParams.SYSTEM_BURNING_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.enable_out_termosstat))) {
+            return StaticParams.OUT_TERMOSTAAT;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.settings_module))) {
+            return StaticParams.SETTINGS_MODEL_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.control_relay))) {
+            return StaticParams.RELAY_PARAMS;
+
+        } else if (mTitle.equals(mContext.getResources().getString(R.string.select_pellet))) {
+            return StaticParams.SELECT_PELET;
+        } else return null;
+    }
+
 
     @Override
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
